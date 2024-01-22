7f2723b94c0daca2033176014b7d042831566fee
c.marescalco <c.marescalco@studenti.unina.it>
2024-01-22 13:28:46 +0100

    exec_from_host upload first

---
+++ exec_from_host.ps1
@@ -0,0 +1,30 @@
+param(
+    [string]$outdir = "output",
+    [string]$path_commands = "example.txt"
+)
+
+mkdir $pwd\$outdir
+
+$VMName = "Malware-VM-Windows"
+$base_path="C:\Users\unina\Desktop\tesi\pwsh-execution-analysis"
+$setup_path = "$base_path\setup.ps1"
+$analysis_path = "$base_path\exec-analysis.ps1"
+
+#Starting the test
+VBoxManage snapshot $VMName restore 81b4953e-36b3-4c49-9950-c5ec905c0316
+VBoxManage startvm $VMName --type headless
+
+Start-Sleep -Seconds 10
+
+VBOxManage guestcontrol $VMName copyto --username unina --password unina --target-directory="$base_path\" $pwd\$path_commands
+VBOxManage guestcontrol $VMName --username unina --password unina run --exe C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe /file $setup_path  --wait-stdout
+VBOxManage guestcontrol $VMName --username unina --password unina run --exe C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe /command "$analysis_path $path_commands > $base_path\log.txt" --wait-stdout
+
+sleep 2
+#saving files
+VBOxManage guestcontrol $VMName copyfrom --username unina --password unina --verbose --recursive --target-directory="$pwd\$outdir" C:\Users\unina\Desktop\tesi\pwsh-execution-analysis\output
+VBOxManage guestcontrol $VMName copyfrom --username unina --password unina --verbose --target-directory="$pwd\$outdir\" C:\Users\unina\Desktop\tesi\pwsh-execution-analysis\log.txt
+
+#save snapshot
+#VBoxManage snapshot "Malware-VM-Windows" take $1 --description "Execution of test $1"
+VBoxManage controlvm $VMName acpipowerbutton --verbose
